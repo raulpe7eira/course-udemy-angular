3d50e6c46989d07429d3cf2d3f71071bc4711b7e
Raul Pereira <raul.pereira@gmail.com>
2017-07-19 19:40:44 -0300

    code for routing-start

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+import { AppRoutingModule } from './app-routing.modules';
+
+import { AuthService } from './auth.service';
+import { ServersService } from './servers/servers.service';
+
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+
+import { ServerResolver } from './servers/server/server-resolver.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent,
+    PageNotFoundComponent,
+    ErrorPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [
+    AuthService,
+    ServersService,
+    AuthGuard,
+    CanDeactivateGuard,
+    ServerResolver
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
