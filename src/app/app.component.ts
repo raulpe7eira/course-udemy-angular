a612ec21c531804988ce3780b0fd29f9a9c9480c
Raul Pereira da Silva <y2kj@petrobras.com.br>
2017-07-19 19:20:14 -0300

    code for basics-assigment-3-start

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  showSecret = false;
+  logs: Array<Date> = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.logs.push(new Date());
+  }
+}
