5a253c9b1cae5b62bd43f0e4525586a45f8c8e6d
Raul Pereira <raul.pereira@gmail.com>
2017-07-19 19:59:22 -0300

    code for pipes-assignment-start

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { SortPipe } from './sort.pipe';
+import { ReversePipe } from './reverse.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SortPipe,
+    ReversePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
