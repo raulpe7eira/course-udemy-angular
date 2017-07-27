4e629b01f943e4bdfbcf93dea676250eaf1009fb
Raul Pereira <raul.pereira@gmail.com>
2017-07-27 18:43:25 -0300

    First animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import { animate, Component, trigger, state, style, transition } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800))
+    ])
+  ]
 })
 export class AppComponent {
-	list = ['Milk', 'Sugar', 'Bread'];
+  list = ['Milk', 'Sugar', 'Bread'];
+  state = 'normal';
 
-	onAdd(item) {
-		this.list.push(item);
-	}
+  onAnimate() {
+    this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+  }
 
-	onDelete(item) {
-		this.list.splice(this.list.indexOf(item), 1);
-	}
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 }
