34a036ce563ec68377e53c64da02bc8daf34abdd
Raul Pereira <raul.pereira@gmail.com>
2017-07-27 19:35:29 -0300

    Animations w/ Keyframes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, Component, trigger, state, style, transition } from '@angular/core';
+import { animate, Component, keyframes, state, style, transition, trigger } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -57,6 +57,40 @@ import { animate, Component, trigger, state, style, transition } from '@angular/
         opacity: 0
       })))
     ]),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void <=> *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('* <=> void', animate(300, style({
+        transform: 'translateX(100px)',
+        opacity: 0
+      })))
+    ])
   ]
 })
 export class AppComponent {
